e90130160f5c142ad19a206b34ed054e7ba4503d
DjDango <168477920+DjDango@users.noreply.github.com>
2024-04-29 22:02:45 +0200

    Update CountUp.py

--- CountUp.py
+++ CountUp.py
@@ -1,7 +1,9 @@
 from time import *
 x = 0 # Put the value in that you want to begin with
 
-while x < 101:
+while x < 101: # Put the value you want x to go to
   print(x)
-  sleep(0.01)
-  x = x + 1
+  sleep(0.01) # Adds x + ... every time, goes up to 0.001 
+  x = x + 1 # x is added by ...
+
+# Hello there
